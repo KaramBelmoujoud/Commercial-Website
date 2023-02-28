c832bc8ce28608270cbf25763c70d105afa2839d
ocotorse <allouchhatim@gmail.com>
2023-02-28 13:01:44 +0100

    sdf

--- src/components/Showprod.js
+++ src/components/Showprod.js
@@ -14,14 +14,24 @@ function Showprod() {
         {product.map((prod) => {
             
             return(
-            <div className='flex'>
-                <div className='flex flex-col items-center w-72  bg-cyan-500 border rounded-xl hover:scale-110 hover:translate-y-1 duration-300'>
-                    <img src={prod.image} alt='' className='w-full h-full bg-white'/>
-                    <h1 className='text-2xl'>{prod.name}</h1>
-                    <h1 className='text-2xl'>{prod.nomlocal}</h1>
-                </div>
+            // <div className='flex'>
+            //     <div className='flex flex-col items-center w-72  bg-cyan-500 border rounded-xl hover:scale-110 hover:translate-y-1 duration-300'>
+            //         <img src={prod.image} alt='' className='w-full h-full bg-white'/>
+            //         <h1 className='text-2xl'>{prod.name}</h1>
+            //         <h1 className='text-2xl'>{prod.nomlocal}</h1>
+            //     </div>
                 
-            </div>
+            // </div>
+            <div class="max-w-sm rounded overflow-hidden h-auto shadow-lg">
+  <img class="w-full" src={prod.image} alt="Sunset in the mountains"/>
+  <div class="px-6 py-4">
+    <div class="font-bold text-xl mb-2">{prod.name}</div>
+  </div>
+  <div class="px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{prod.nomlocal}</span>
+
+  </div>
+</div>
             )
         })}
       </div>
