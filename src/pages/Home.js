35b177fb7963e6e8e0ce1f9147b9f1cca462a7d9
ocotorse <allouchhatim@gmail.com>
2023-03-02 14:28:37 +0100

    sjhdgf

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { keyframes } from "styled-components";
 import  product  from '../assets/data/products'
 import pesca from '../assets/pes.png'
@@ -8,43 +8,26 @@ import AboutUs from '../components/AboutUs';
 import Footer from '../components/Footer';
 import Entreheader from '../components/Entreheader';
 import Services from '../components/services';
+import Prodslide from '../components/prodslide';
+
+
 
 
 function Home() {
   
   return (
-    <div className='overflow-hidden'>
-      
+    <div className='overflow-x-hidden overflow-hidden bg-slate-50 scrollbar-hide'>
      <div className='flex justify-center content-center '>
-   <img src={pesca} alt="logo" className="h-64 w-52 absolute  mt-9 " />
     <h1 className='text-6xl absolute text-center ml-8  mt-72'>Pescatitan</h1>
        </div>  
           <Entreheader />
           <AboutUs />
-          <Services />
-          <div className='left-96 right-96'>
-          <Carousel navButtonsAlwaysVisible='true'>
-          {
-          slides.map((prod) => 
-          
-        
-             
-              <div class="max-w-sm rounded overflow-hidden h-auto shadow-lg m-auto">
-              <img class="w-full" src={prod.image} alt="Sunset in the mountains"/>
-              <div class="px-6 py-4">
-                <div class="font-bold text-2xl mb-2">{prod.title}</div>
-              </div>
-              <div class="px-6 pt-4 pb-2">
-                <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{prod.description}</span>
-              </div>
-            </div>
-            
-            
-          
-         )
-        }         
-          </Carousel>
-
+          <div className='flex flex-col justify-between items-start  mb-9'>
+          <span class="w-full h-2 rounded-xl bg-blue-900 ml-5 lg:w-2/3 "/>
+          </div>
+          <Prodslide/>
+          <div className='flex flex-col justify-between items-end '>
+          <span class="w-full h-2 rounded-xl bg-blue-900 mr-5 lg:w-2/3 "/>
           </div>
           <Services />
           {/* <ContactUs /> */}
