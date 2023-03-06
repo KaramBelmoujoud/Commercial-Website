46275a2576b86c872c78f3d69915a133306bcdd8
StyrO <karam.belmoujoud@e-polytechnique.ma>
2023-03-06 12:22:29 +0100

    dz

--- src/App.js
+++ src/App.js
@@ -153,7 +153,7 @@ function App() {
   return (
 
     <div >
-      <HashRouter basename='/'>
+      <HashRouter baseline='/'>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/Products" element={<Products />} />
